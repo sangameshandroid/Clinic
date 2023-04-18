4373664da9275e8ca61f183e6b385f0d04d79723
user1 <sangameshp37@gmail.com>
2023-04-18 17:11:36 +0530

    project finished

--- app/src/main/java/com/example/clinic/RegisterAllAdminFragment.java
+++ app/src/main/java/com/example/clinic/RegisterAllAdminFragment.java
@@ -1,12 +1,19 @@
 package com.example.clinic;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,30 +21,49 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
-import java.util.HashMap;
-import java.util.Map;
 
-
+@SuppressWarnings("ALL")
 public class RegisterAllAdminFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private static final int PICK_IMAGE_CODE = 1000;
     private Spinner languagesspinner, nationalityspinner, cityspinner, usertypespinner, specilizationspinner, genderspinner;
     Button btn_user_register;
     EditText register_first_name, register_last_name, register_user_email, register_user_mobile, register_user_date,register_user_zip, regsiter_user_id, register_user_password, register_user_address, register_user_bio;
+    ImageView img_upload;
+    TextView txt_upload;
 
     boolean valid = true;
     // Firebase
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+
+    Uri filePath;
+    Bitmap bitmap;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    ProgressDialog progressDialog;
+    StorageReference storageReference;
 
 
     public RegisterAllAdminFragment() {
@@ -50,6 +76,39 @@ public class RegisterAllAdminFragment extends Fragment implements AdapterView.On
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_CODE){
+            UploadTask uploadTask = storageReference.putFile(data.getData());
+
+            Task<Uri> task = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        Toast.makeText(getContext(), "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                    return storageReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        String url = task.getResult().toString().substring(0, task.getResult().toString().indexOf("token"));
+                        Log.d("DIRECTLINK", url);
+
+                        Picasso.get().load(url).into(img_upload);
+
+
+
+                    }
+                }
+            });
+
+        }
 
     }
 
@@ -80,73 +139,23 @@ public class RegisterAllAdminFragment extends Fragment implements AdapterView.On
         register_user_password = view.findViewById(R.id.register_user_password);
         register_user_address = view.findViewById(R.id.register_user_address);
         register_user_bio = view.findViewById(R.id.register_user_bio);
+        img_upload = view.findViewById(R.id.img_upload);
+        txt_upload = view.findViewById(R.id.txt_upload);
 
         //FireBase
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
 
+        firebaseAuth = FirebaseAuth.getInstance();
 
-
-        btn_user_register.setOnClickListener(new View.OnClickListener() {
+        storageReference = FirebaseStorage.getInstance().getReference("image_upload");
+        txt_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkFeild(register_first_name);
-                checkFeild(register_last_name);
-                checkFeild(register_user_email);
-                checkFeild(register_user_mobile);
-                checkFeild(register_user_date);
-                checkFeild(register_user_zip);
-                checkFeild(regsiter_user_id);
-                checkFeild(register_user_password);
-                checkFeild(register_user_address);
-                checkFeild(register_user_bio);
-
-                if (valid) {
-                    fAuth.createUserWithEmailAndPassword(register_user_zip.getText().toString(), register_user_password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            FirebaseUser user = fAuth.getCurrentUser();
-                            Toast.makeText(getContext(), "User Account Created", Toast.LENGTH_SHORT).show();
-                            DocumentReference df = fStore.collection("Users").document(user.getUid());
-                            Map<String, Object> userInfo = new HashMap<>();
-
-
-                            ///
-                            /*userInfo.put("Language", languagesspinner.toString());
-                            userInfo.put("Nationality", nationalityspinner.toString());
-                            userInfo.put("City", cityspinner.toString());
-                            userInfo.put("UserType", usertypespinner.toString());
-                            userInfo.put("Gender", genderspinner.toString());
-                            userInfo.put("Specilization", specilizationspinner.toString());*/
-                            ///
-
-                            userInfo.put("FirstName", register_first_name.getText().toString());
-                            userInfo.put("LastName", register_last_name.getText().toString());
-                            userInfo.put("Email", register_user_email.getText().toString());
-                            userInfo.put("Mobile", register_user_mobile.getText().toString());
-                            userInfo.put("DateBirth", register_user_date.getText().toString());
-                            userInfo.put("Zip", register_user_zip.getText().toString());
-                            userInfo.put("UserId", regsiter_user_id.getText().toString());
-                            userInfo.put("UserPassword", register_user_password.getText().toString());
-                            userInfo.put("Address", register_user_address.getText().toString());
-                            userInfo.put("Bio", register_user_bio.getText().toString());
-                            userInfo.put("isUser", "1");
-
-                            df.set(userInfo);
-
-
-
-
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getContext(), "User Account Is not Created", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                }
-
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent.createChooser(intent,"Select Picture"), PICK_IMAGE_CODE);
 
             }
         });
@@ -154,27 +163,104 @@ public class RegisterAllAdminFragment extends Fragment implements AdapterView.On
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      /*  btn_user_register.setOnClickListener(new View.OnClickListener() {
+        btn_user_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Registered The User Succesfully", Toast.LENGTH_SHORT).show();
+                String firstname = register_first_name.getText().toString();
+                String lastname = register_last_name.getText().toString();
+                String email = register_user_email.getText().toString();
+                String mobile = register_user_mobile.getText().toString();
+                String date = register_user_date.getText().toString();
+                String zip = register_user_zip.getText().toString();
+                String userid = regsiter_user_id.getText().toString();
+                String userpassword = register_user_password.getText().toString();
+                String address = register_user_address.getText().toString();
+                String bio = register_user_bio.getText().toString();
+// spinner
+                String gender = genderspinner.getSelectedItem().toString();
+                String language = languagesspinner.getSelectedItem().toString();
+                String nationality = nationalityspinner.getSelectedItem().toString();
+                String city = cityspinner.getSelectedItem().toString();
+                String usertype = usertypespinner.getSelectedItem().toString();
+                String specilization = specilizationspinner.getSelectedItem().toString();
+
+
+
+                if (TextUtils.isEmpty(userid)) {
+                    Toast.makeText(getContext(), "Please enter a valid user ID", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(userpassword)) {
+                    Toast.makeText(getContext(), "Please enter a valid password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(userid).matches()) {
+                    Toast.makeText(getContext(), "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String languageSelected = "";
+                String genderSelected = "";
+                String nationalitySelected = "";
+                String citySelected = "";
+                String userTypeSelected = "";
+                String specializationSelected = "";
+
+// Get the selected values from the spinners
+                if (genderspinner != null && genderspinner.getSelectedItem() != null) {
+                    genderSelected = genderspinner.getSelectedItem().toString();
+                }
+
+                if (languagesspinner != null && languagesspinner.getSelectedItem() != null) {
+                    languageSelected = languagesspinner.getSelectedItem().toString();
+                }
+
+                if (nationalityspinner != null && nationalityspinner.getSelectedItem() != null) {
+                    nationalitySelected = nationalityspinner.getSelectedItem().toString();
+                }
+
+                if (cityspinner != null && cityspinner.getSelectedItem() != null) {
+                    citySelected = cityspinner.getSelectedItem().toString();
+                }
+
+                if (usertypespinner != null && usertypespinner.getSelectedItem() != null) {
+                    userTypeSelected = usertypespinner.getSelectedItem().toString();
+                }
+
+                if (specilizationspinner != null && specilizationspinner.getSelectedItem() != null) {
+                    specializationSelected = specilizationspinner.getSelectedItem().toString();
+                }
+
+
+                String finalSpecializationSelected = specializationSelected;
+                String finalUserTypeSelected = userTypeSelected;
+                String finalCitySelected = citySelected;
+                String finalNationalitySelected = nationalitySelected;
+                String finalLanguageSelected = languageSelected;
+                String finalGenderSelected = genderSelected;
+                firebaseAuth.createUserWithEmailAndPassword(userid, userpassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(getContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
+
+                        rootNode = FirebaseDatabase.getInstance();
+                        reference = rootNode.getReference("users");
+                        if (usertype != null && !usertype.isEmpty()) { // Check if usertype is not null or empty
+
+                            dataholder dholder = new dataholder(firstname, lastname, email, mobile, date, zip, userid, userpassword, address, bio, finalGenderSelected, finalLanguageSelected, finalNationalitySelected, finalCitySelected, finalUserTypeSelected, finalSpecializationSelected);
+                            reference.child(usertype).push().setValue(dholder);
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
             }
-        });*/
+        });
 
         ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getContext(), R.array.Languages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -219,9 +305,9 @@ public class RegisterAllAdminFragment extends Fragment implements AdapterView.On
 
         genderspinner.setOnItemSelectedListener(this);
 
-
         return view;
     }
+
     public boolean checkFeild(EditText textFeild){
         if (textFeild.getText().toString().isEmpty()){
             textFeild.setError("Error");
@@ -241,14 +327,13 @@ public class RegisterAllAdminFragment extends Fragment implements AdapterView.On
         String usertype = adapterView.getItemAtPosition(i).toString();
 
 
-
-
-
-
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private class READ_EXTERNAL_STORAGE {
     }
 }
---
+++ app/src/main/java/com/example/clinic/dataholder.java
@@ -0,0 +1,158 @@
+package com.example.clinic;
+
+public class dataholder {
+    String firstname, lastname, email, mobile, date, zip, username, userpassword, address, bio, gender, language,nationality, city, usertype, specilization;
+
+
+    public dataholder(String firstname, String lastname, String email, String mobile, String date, String zip, String username, String userpassword, String address, String bio, String gender, String language, String nationality, String city, String usertype, String specilization) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.mobile = mobile;
+        this.date = date;
+        this.zip = zip;
+        this.username = username;
+        this.userpassword = userpassword;
+        this.address = address;
+        this.bio = bio;
+        this.gender = this.gender;
+        this.language = this.language;
+        this.nationality = this.nationality;
+        this.city = this.city;
+        this.usertype = this.usertype;
+        this.specilization = this.specilization;
+
+    }
+
+    public dataholder() {
+
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getSpecilization() {
+        return specilization;
+    }
+
+    public void setSpecilization(String specilization) {
+        this.specilization = specilization;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
